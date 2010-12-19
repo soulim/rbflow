0a7e62797e0c6007f40f565f03dead843bdf9160
Alex Soulim <soulim@gmail.com>
2010-12-19 13:02:33 +0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rbflow::Application.config.secret_token = '6b48bbbe40ecd2af009e14cf473f858aefa9bea00502c2097f02049c5f93874fe686f585a42892bb9b87c6c33cc530fce8b43f04b68c8fc689e5251bc235e03c'

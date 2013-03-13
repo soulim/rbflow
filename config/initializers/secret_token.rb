8cb087399c6f26e5b2d01a0abdc77a69d256263c
Alex Soulim <soulim@gmail.com>
2013-03-13 12:15:06 +0400

    Hide session token from public

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Rbflow::Application.config.secret_token = '947fd605e901aa20a3724a0642f10fd1057ed4a79275144f954542220d89b172806ef87bbc71ac9746366396daff396302223681fb8f7aa5ff5fa010a015b515'
+Rbflow::Application.config.secret_token = ENV['SESSION_SECRET'] || 'afake32charactertokenfortesting'

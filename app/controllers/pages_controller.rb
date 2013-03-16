7242ff5fa6b5563ec15e5042d71e46d65b51e2b8
Alex Soulim <soulim@gmail.com>
2013-03-16 18:05:05 +0400

    Add the DigitalOcean badge

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,8 @@
 class PagesController < ApplicationController
   respond_to :html
-  
+
   def show
     @page = params[:id]
-    render(:layout => false, :file => "public/404.html", :status => :not_found) unless %w(about contacts terms).include?(@page)
+    render(:layout => false, :file => "public/404.html", :status => :not_found) unless %w(about).include?(@page)
   end
 end
